2cb7298679d3751e6200a982b6cdc507afb3bca4
Tim OFallon <timothy.p.ofallon@gmail.com>
2020-07-29 12:26:41 -0400

    Would you rather game NSFW

---
+++ would_you_rather.py
@@ -0,0 +1,60 @@
+#!/usr/bin/bash/env python3
+
+round = 0
+while round <= 4:
+    round = round + 1
+    a = "a-eat dog poo, "
+    b = "b-see your mom naked?"
+    print(f"Would you rather {a} or {b}")
+    answer1 = input("a or b---")
+    if answer1.lower() == "a":
+        print("good call!")
+        break
+    if answer1.lower() == "b":
+        print("Are you from Alabama?")
+        break
+    else:
+        print("That was neither a nor b! You just lost a turn!")
+
+while round <= 4:
+    round = round + 1
+    a = "stub your toe, "
+    b = "be naked in front of a crowd?"
+    print(f"Would you rather {a} or {b} -")
+    answer1 = input("a or b---")
+    if answer1.lower() == "a":
+        print("Ouch! But still a good call.")
+        break
+    if answer1.lower() == "b":
+        print("Yuck, pervert.")
+        break
+    else:
+        print("That was neither a nor b! You just lost a turn!")
+while round <= 4:
+    round = round + 1
+    a = "Watch Bambi\'s Mom get murked by a hunter, "
+    b = "Watch Simba\'s Dad get repeatedly trampled by a heard of Wilderbeast because of that murderer Mufasa? "
+    print(f"Would you rather {a} or {b} - ")
+    answer1 = input("a or b---")
+    if answer1.lower() == "a":
+        print("Good call, that is called the circle of life and good eatin!")
+        break
+    if answer1.lower() == "b":
+        print("Wow, someone should report you to the FBI for being a potential serial killer, ya psycho!")
+        break
+while round <= 4:
+    round = round + 1
+    a = "Perform coitus on Hillary Clinton, "
+    b = "Perform fellatio on Bill Clinton? "
+    print(f"Would you rather {a} or {b} - ")
+    answer1 = input("a or b---")
+    if answer1.lower() == "a":
+        print("Just....idk....I cant even...")
+        print("The game is now over, go see a Therapist...")
+        break
+    if answer1.lower() == "b":
+        print("Just....idk....I cant even...")
+        print("The game is now over, go see a Therapist...")
+        break
+
+
